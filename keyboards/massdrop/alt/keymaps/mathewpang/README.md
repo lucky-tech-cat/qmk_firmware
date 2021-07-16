ea5a53a9e0cd3d442bbc15e00b88505bc50d0506
Mathew Pang <mpang@sentient-energy.com>
2021-07-15 18:38:00 -0700

    Added Custom Keymap for Drop Alt

---
+++ keyboards/massdrop/alt/keymaps/mathewpang/README.md
@@ -0,0 +1,14 @@
+### Drop (Massdrop) ALT Layout
+
+This layout is for the [Drop ALT Keyboard](https://drop.com/buy/massdrop-alt-high-profile-mechanical-keyboard).
+
+Based on the emptyflask layout
+
+Features:
+
+* Prefer grave/tilde to dedicated ESC key
+* Numpad layer (FN-\ to enable)
+* Method for clearing all stuck-down mods (taken from favorable-mutation, for tapped modifiers)
+
+To do:
+* Customize RGB: solid colors by default, highlight numpad keys when using that layer.
---
+++ keyboards/massdrop/alt/keymaps/mathewpang/config.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#define ONESHOT_TIMEOUT 3000
+
+#define RGB_MATRIX_FRAMEBUFFER_EFFECTS
+
+#define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_SOLID_MULTISPLASH // Sets the default mode, if none has been set
+#define RGB_MATRIX_STARTUP_HUE 148 //AZURE // Sets the default hue value, if none has been set
+#define RGB_MATRIX_STARTUP_SAT 255 // 0 to 255 // Sets the default saturation value, if none has been set
+#define RGB_MATRIX_STARTUP_VAL RGB_MATRIX_MAXIMUM_BRIGHTNESS // Sets the default brightness value, if none has been set
+#define RGB_MATRIX_STARTUP_SPD 127 // Sets the default animation speed, if none has been set`
